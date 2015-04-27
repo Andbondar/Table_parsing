331ea9a9c89f5bd1c40e66ff3cfe0998a6a1b5ec
Andbondar <andbondar@yandex.ru>
2015-04-27 15:13:59 +0300

    added cell class

--- src/main/java/Table/Cell.java
+++ src/main/java/Table/Cell.java
@@ -2,6 +2,10 @@ package Table;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by bondar on 4/27/2015.
@@ -19,6 +23,24 @@ public class Cell {
 
     public String getText(){
         return driver.findElement(By.xpath(".//")).getText();
+    }
+
+    public List<WebElement> getLinks(){
+        ArrayList<WebElement> returnedList = new ArrayList<>();
+        int size = driver.findElements(By.xpath(".//a")).size();
+        for (int i = 0; i<size; i++){
+            returnedList.add(driver.findElements(By.xpath(".//a")).get(i));
+        }
+        return returnedList;
+    }
+
+    public List<WebElement> getImages(){
+        ArrayList<WebElement> returnedList = new ArrayList<>();
+        int size = driver.findElements(By.xpath(".//img")).size();
+        for (int i = 0; i<size; i++){
+            returnedList.add(driver.findElements(By.xpath(".//img")).get(i));
+        }
+        return returnedList;
     }
 
 }
